5a57d104c8d9b96ecf2ef460538316fabd582432
Unik-Dahal <unikdahal03@gmail.com>
2022-01-10 17:14:02 +0545

    Amazing Pattern Problems

---
+++ Apna College C++/Awesome Pattern Questions/problem1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 4; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
