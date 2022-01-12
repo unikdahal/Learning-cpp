bef66478f66d1a1f47af6309c3d49377b77079bb
Unik-Dahal <unikdahal03@gmail.com>
2022-01-12 21:38:49 +0545

    Triangle

---
+++ Apna College C++/Awesome Pattern Questions/problem8.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << " *";
+        }
+        cout << endl;
+    }
+    for (int i = 3; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << " *";
+        }
+        cout << endl;
+    }
+}
