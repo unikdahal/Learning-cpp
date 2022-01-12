96df8d5adb9decdee6ab7a0d07c444f6dede7b85
Unik-Dahal <unikdahal03@gmail.com>
2022-01-12 21:45:24 +0545

    Inverted Number

---
+++ Apna College C++/Awesome Pattern Questions/problem9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    for (int i = 5; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+            n++;
+        }
+        cout << endl;
+    }
+}
