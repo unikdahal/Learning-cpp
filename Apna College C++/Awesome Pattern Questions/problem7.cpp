64f8af4fc20aed2b19d1a0c7418dd9c25a2ffdcd
Unik-Dahal <unikdahal03@gmail.com>
2022-01-12 21:25:33 +0545

    Butterfly Pattern

--- Apna College C++/Awesome Pattern Questions/problem7.cpp
+++ Apna College C++/Awesome Pattern Questions/problem7.cpp
@@ -36,3 +36,43 @@ int main()
         cout << endl;
     }
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+
+//     for (int i = 1; i < n; i++)
+//     {
+//         for (int j = 1; j < 2 * n - 1; j++)
+//         {
+//             if (i >= j || i > 2 * n - 2 - j)
+//             {
+//                 cout << "* ";
+//             }
+//             else
+//             {
+//                 cout << "  ";
+//             }
+
+//     //         }
+//     //         cout<<endl;
+//     //     }
+//     //     for(int i=n-1;i>=1;i--){
+//     //         for(int j=1 ;j<2*n-1;j++)
+//     //         {
+//     //             if(i>=j ||i>2*n-2-j )
+//     //             {
+//     //                 cout<<"* ";
+//     //             }
+//     //             else{
+//     //                 cout<<"  ";
+//     //             }
+//     //         }
+//     //         cout<<endl;
+//     //     }
+//     //     return 0;
+//     // }
