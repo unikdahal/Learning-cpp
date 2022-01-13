2d898c97e286f7050bffcbd72729d06a8d17eebf
Unik-Dahal <unikdahal03@gmail.com>
2022-01-14 00:31:38 +0545

    Reverse of a Number

---
+++ Apna College C++/Awesome Pattern Questions/problem15.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n, reverse = 0;
+    cin >> n;
+    while (n > 0)
+    {
+        int lastdigit = n % 10;
+        reverse = reverse * 10 + lastdigit;
+        n = n / 10;
+    }
+
+    cout << reverse;
+    return 0;
+}
