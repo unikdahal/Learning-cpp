a20db94cbbb4ec05e2e3f0acbcccf68d39131f17
Unik-Dahal <unikdahal03@gmail.com>
2022-01-13 23:51:08 +0545

    Star Pattern

---
+++ Apna College C++/Awesome Pattern Questions/problem14.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 4; j >= i; j--)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= (2 * i - 1); k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= (n - i); j++)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= (2 * i - 1); k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
